11a16e29995b890e28dc69d7334d44aa03358c16
z-mathews <zenon.mathews@vtg.admin.ch>
2025-03-24 06:55:51 +0100

    replay sampling_time can now be a float instead of int

--- openburst/constants/replayconstants.py
+++ openburst/constants/replayconstants.py
@@ -7,8 +7,8 @@ REF_REPLAY_REPORT = 5471 # communication key with the client
 # [s] default replay data DB write interval, this will be overwritten by client entry
 REPLAY_DATA_DB_WRITE_INTERVAL = 1.0
 
-# [s] default sampling from the track file (ref and test), this will be overwritte by the client
-SAMPLING_TIME = 2
+# [s] default sampling from the track file (ref and test), this will be overwritten by the client
+SAMPLING_TIME = 0.1 # 2
 
 # [s] sampling time when writing new user designed targets
-NEW_TARGET_SAMPLING_TIME = 10
+NEW_TARGET_SAMPLING_TIME = 0.1 # 10
